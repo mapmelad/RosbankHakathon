52a0ec9e668b5fe91b79638b1b9d8a2b63b8b4e9
own2pwn <7850039+own2pwn@users.noreply.github.com>
2018-11-17 17:11:22 +0300

    feed elements

---
+++ RosbankHackathon/RosbankHackathon/Module/Feed/Offer/View/OfferFeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  OfferFeedCell.swift
+//  RosbankHackathon
+//
+//  Created by Evgeniy on 17/11/2018.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+final class OfferFeedCell: UICollectionViewCell {
+    // MARK: - Outlets
+
+    
+}
