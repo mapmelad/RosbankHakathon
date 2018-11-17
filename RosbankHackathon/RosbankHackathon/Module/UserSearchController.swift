600bf0c55eabc0c488104652b8cee04d6056e7d3
own2pwn <7850039+own2pwn@users.noreply.github.com>
2018-11-17 15:37:24 +0300

    v1

---
+++ RosbankHackathon/RosbankHackathon/Module/UserSearchController.swift
@@ -0,0 +1,23 @@
+//
+//  UserSearchController.swift
+//  RosbankHackathon
+//
+//  Created by Evgeniy on 17/11/2018.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+final class UserHeaderView: UICollectionReusableView {
+    // MARK: - Outlets
+
+    
+}
+
+final class UserSearchController: UIViewController {
+    // MARK: - Outlets
+
+    @IBOutlet
+    private var userCollection: UICollectionView!
+
+}
